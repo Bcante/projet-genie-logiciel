becc069be2f22c5732de19939f68f31eb5523842
benitopepito <cantebenoit@hotmail.com>
2018-04-19 21:47:14 +0200

    Test sur sérialisation

---
+++ src/test/testSerial.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import metroproject.Metro;
+import metroproject.Rail;
+import metroproject.Station;
+import org.junit.jupiter.api.Test;
+import utilitaires.GestionnaireSauvegarde;
+
+class testSerial {
+
+    @Test
+    void serializationDeserialization() {
+        Metro grandMetro = new Metro();
+        grandMetro.addStation("Paris");
+        grandMetro.addStation("Berlin");
+
+        grandMetro.addRail(grandMetro.getStations().get(0),grandMetro.getStations().get(1),5,false);
+
+        GestionnaireSauvegarde.sauvegarde(grandMetro);
+
+        Metro clone = GestionnaireSauvegarde.chargement();
+        assertNotNull(clone);
+        assertEquals(2,clone.getStations().size());
+        assertEquals(1,clone.getRails().size());
+    }
+}
