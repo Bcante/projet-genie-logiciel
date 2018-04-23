7d919e0789ff98ba5e79cdc803aac2a83340eef1
benitopepito <cantebenoit@hotmail.com>
2018-04-23 18:12:52 +0200

    Implémentation méthode pour trouver la station la plus proche

---
+++ src/main/java/metroproject/Geolocalisation.java
@@ -0,0 +1,38 @@
+package metroproject;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+
+public class Geolocalisation {
+
+    private Map<String,Station> stations;
+
+    public Geolocalisation(Map stations) {
+        this.stations = stations;
+    }
+
+    public String trouveStationProche(Utilisateur u) {
+        Point depart = new Point(u.getX(),u.getY());
+        Point dest = null;
+        Station laPlusProche = null;
+        int distance = -1;
+
+        for (Map.Entry<String, Station> entry : stations.entrySet())
+        {
+            Station stationTmp = entry.getValue();
+
+            dest = new Point(stationTmp.getX(),stationTmp.getY());
+            int distanceTmp = (int) depart.distanceSq(dest);
+
+            if (laPlusProche == null || distance > distanceTmp) {
+                distance = distanceTmp;
+                laPlusProche = stationTmp;
+            }
+        }
+
+        String res = laPlusProche.getNomStation();
+        return res;
+    }
+}
