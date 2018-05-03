eef5e516fcb884dc04f4e5e03c6bfcf4dd56d421
Holliehl <rowane.k@gmail.com>
2018-05-03 13:28:59 +0200

    ajouts de tests

--- src/test/java/test/testRechercheChemin.java
+++ src/test/java/test/testRechercheChemin.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import Main.Plan;
 import metroproject.Metro;
 import metroproject.PathComparator;
+import metroproject.PathFinder;
 import metroproject.Station;
 
 public class testRechercheChemin {
 	static Plan p=new Plan();
 	static Metro m;
 	PathComparator comparator=new PathComparator();
-	
+	PathFinder pf=new PathFinder();
 	
 	@BeforeAll
 	static void beforeAll() {
@@ -77,5 +79,28 @@ public class testRechercheChemin {
 		path2.add(m.getStation("CharlesDeGaulle"));
 		assertEquals(-1,comparator.compare(path1, path2));
 	}
+	
+	@Test
+	@DisplayName("Un chemin est bien retourné")
+	void returnsPath() {
+		ArrayList<Station> res=null;
+		res=pf.shortestPath(m.getStation("AlexandreDumas"), m.getStation("Argentine"));
+		assertNotNull(res);
+	}
+	
+	@Test
+	@DisplayName("Vérification que le chemin trouvé est bien le plus rapide.")
+	void isFastest() {
+		//Chemin le + rapide d'Esplanade à Barbès: E-> FranklinDRoosevelt-> Colonnel Fabien -> Barbes
+		ArrayList<Station> res=null;
+		ArrayList<Station> expected=new ArrayList<Station>();
+		expected.add(m.getStation("Esplanade"));
+		expected.add(m.getStation("FranklinDRoosvlet"));
+		expected.add(m.getStation("ColonnelFabien"));
+		expected.add(m.getStation("Barbes"));
+		res=pf.shortestPath(m.getStation("Esplanade"), m.getStation("Barbes"));
+		assertEquals(res,expected);
+	}
+	
 
 }
