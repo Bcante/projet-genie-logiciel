5f55f3bced11a873942a45f834335ba4581709a1
Holliehl <rowane.k@gmail.com>
2018-05-18 17:52:49 +0200

    Ajouts de tests pour l'itinéraire avec le moins de changements

--- src/test/java/test/testChangementsLignes.java
+++ src/test/java/test/testChangementsLignes.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.ArrayList;
 
@@ -43,4 +44,45 @@ public class testChangementsLignes {
 		path2.add(m.getStation("CharlesDeGaulle"));
 		assertEquals(1,comparator.compare(path1, path2));
 	}
+	
+	@Test
+	void testComparatorLinesSameNbOfChanges() {
+		ArrayList<Station> path1=new ArrayList<Station>();
+		ArrayList<Station> path2=new ArrayList<Station>();
+		path1.add(m.getStation("Argentine"));
+		path1.add(m.getStation("Bastille"));
+		path1.add(m.getStation("CharlesDeGaulle"));
+		path1.add(m.getStation("ColonnelFabien"));
+		
+		path2.add(m.getStation("AlexandreDumas"));
+		path2.add(m.getStation("Anvers"));
+		path2.add(m.getStation("Esplanade"));
+		path2.add(m.getStation("FranklinDRoosvlet"));
+		path2.add(m.getStation("Avron"));
+		path2.add(m.getStation("CharlesDeGaulle"));
+		path2.add(m.getStation("Berault"));
+		assertEquals(0,comparator.compare(path1, path2));
+	}
+	
+	@Test
+	void returnsPath() {
+		ArrayList<Station> res=new ArrayList<Station>();
+		res=pf.leastLineChange(m.getStation("AlexandreDumas"), m.getStation("Argentine"));
+		assertNotNull(res);
+	}
+	
+	@Test
+	void returnsLeastChangePath() {
+		ArrayList<Station> res=pf.leastLineChange(m.getStation("FranklinDRoosvlet"), m.getStation("Concorde"));
+		ArrayList<Station> expected=new ArrayList<Station>();
+		expected.add(m.getStation("FranklinDRoosvlet"));
+		expected.add(m.getStation("Esplanade"));
+		expected.add(m.getStation("Courcelles"));
+		expected.add(m.getStation("Concorde"));
+		for (Station s: res) {
+			System.out.println(s.getNomStation());
+		}
+		assertEquals(res,expected);
+	}
+	
 }
